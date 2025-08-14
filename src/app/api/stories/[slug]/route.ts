0224e8709d68713b87e016d388701181ae8ec15b
JoshMuasya <muasyajoshua07@gmail.com>
2025-08-15 00:33:00 +0300

    Added admin panel3

--- src/app/api/stories/[slug]/route.ts
+++ src/app/api/stories/[slug]/route.ts
@@ -47,10 +47,10 @@ export async function DELETE(request: Request, { params }: { params: { slug: str
 // GET: Get specific Story
 export async function GET(
   request: NextRequest,
-  context: { params: { slug: string } }
+  { params }: { params: { slug: string } }
 ) {
   try {
-    const { slug } = context.params;
+    const { slug } = params;
 
     if (!slug) {
       return NextResponse.json(
